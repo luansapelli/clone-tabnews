62ff9a9fe2c6906a70d4fa28b105cbf4dfb006c3
Luan <42867942+luansapelli@users.noreply.github.com>
2024-08-02 20:44:38 -0300

    move client.connect to try block

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,13 @@ async function query(queryObject) {
     password: env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
