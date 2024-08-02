e92dc6917db0bdf943c30cf89585fc8399790e5d
Luan <42867942+luansapelli@users.noreply.github.com>
2024-08-01 22:24:08 -0300

    add max connections and opened connections from database to status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
-import database from "infra/database.js";
+import database from "infra/database";
+import { env } from "process";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [dbName],
+  });
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    key: "value",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
   });
 }
 
